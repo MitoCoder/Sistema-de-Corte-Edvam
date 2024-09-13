e98e7e5066ea65226151c4d966d9d544468e9e59
Edvam Santos <edvamsantos1998@gmail.com>
2024-09-13 13:25:09 -0300

    Adicionado Novas Funções
    -Calculo e Soma Por QTD. de Folhas.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [paperHeight, setPaperHeight] = useState(96);
   const [cutWidth, setCutWidth] = useState(66);
   const [cutHeight, setCutHeight] = useState(33);
+  const [paperQuantity, setPaperQuantity] = useState(1);
 
   const calculateCuts = () => {
     const rows = Math.floor(paperHeight / cutHeight);
@@ -19,7 +20,7 @@ const App = () => {
     const leftoverWidth = paperWidth - totalCutWidth;
     const leftoverHeight = paperHeight - totalCutHeight;
 
-    return { rows, cols, leftoverWidth, leftoverHeight };
+    return { rows, cols, leftoverWidth, leftoverHeight, totalCuts: rows * cols };
   };
 
   const drawPlan = () => {
@@ -27,7 +28,7 @@ const App = () => {
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const { rows, cols, leftoverWidth, leftoverHeight } = calculateCuts();
+    const { rows, cols, leftoverWidth, leftoverHeight, totalCuts } = calculateCuts();
     const cutW = (paperWidth / cols) * 10; // Fator de escala para visualização
     const cutH = (paperHeight / rows) * 10;
 
@@ -64,44 +65,56 @@ const App = () => {
 
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1; // Linha padrão para os outros detalhes
+
+    ctx.font = '08px Arial';
+    ctx.fillText(`Total Após Corte: ${totalCuts * paperQuantity}`, 10, 20 + paperHeight * 10);
   };
 
   const generatePDF = () => {
-    const { rows, cols, leftoverWidth, leftoverHeight } = calculateCuts();
+    const { rows, cols, leftoverWidth, leftoverHeight, totalCuts } = calculateCuts();
     const pdf = new jsPDF();
 
     const pageWidth = pdf.internal.pageSize.getWidth();
-    const cutW = 180 / paperWidth;  // Fator de escala para o PDF
-    const cutH = 260 / paperHeight; // Fator de escala para o PDF
+    const cutW = 160 / paperWidth;  // Fator de escala para o PDF
+    const cutH = 200 / paperHeight; // Fator de escala para o PDF
     const centerX = (pageWidth - cutW * paperWidth) / 2; // Centralizar cortes no PDF
 
+    // Adiciona o título do PDF
+    pdf.setFontSize(13);
     pdf.text(`Plano de Corte - Papel: ${paperWidth}x${paperHeight} cm`, centerX, 10);
 
+    // Adiciona o total de folhas e o total após corte
+    pdf.setFontSize(10);
+    pdf.text(`Total de Folhas: ${paperQuantity}`, centerX, 15);
+    pdf.text(`Total após corte: ${totalCuts * paperQuantity}`, centerX, 20);
+
+    // Desenha os cortes
     pdf.setDrawColor(0, 0, 0); // Preto para as linhas de corte
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        pdf.rect(centerX + j * cutW * cutWidth, 20 + i * cutH * cutHeight, cutW * cutWidth, cutH * cutHeight);
-        pdf.text(`${cutWidth}x${cutHeight}`, centerX + 2 + j * cutW * cutWidth, 25 + i * cutH * cutHeight);
+        pdf.rect(centerX + j * cutW * cutWidth, 30 + i * cutH * cutHeight, cutW * cutWidth, cutH * cutHeight);
+        pdf.text(`${cutWidth}x${cutHeight}`, centerX + 2 + j * cutW * cutWidth, 45 + i * cutH * cutHeight);
       }
     }
 
+    // Desenha as sobras
     pdf.setDrawColor(139, 0, 0); // Vermelho escuro para a linha da sobra
     pdf.setFont("helvetica", "bold"); // Fonte em negrito para o texto
 
     if (leftoverWidth > 0) {
-      pdf.rect(centerX + cols * cutW * cutWidth, 20, cutW * leftoverWidth, cutH * paperHeight);
+      pdf.rect(centerX + cols * cutW * cutWidth, 30, cutW * leftoverWidth, cutH * paperHeight);
       const textX = centerX + cols * cutW * cutWidth + cutW * leftoverWidth + 5;
-      const textY = 30 + cutH * paperHeight / 2;
+      const textY = 23 + cutH * paperHeight / 2;
       pdf.text(`Sobra: ${leftoverWidth} cm`, textX, textY, { angle: 90 });
     }
     if (leftoverHeight > 0) {
-      pdf.rect(centerX, 20 + rows * cutH * cutHeight, cutW * paperWidth, cutH * leftoverHeight);
+      pdf.rect(centerX, 30 + rows * cutH * cutHeight, cutW * paperWidth, cutH * leftoverHeight);
       const textX = centerX + 2;
-      const textY = 30 + rows * cutH * cutHeight + cutH * leftoverHeight - 5;
+      const textY = 40 + rows * cutH * cutHeight + cutH * leftoverHeight - 5;
       pdf.text(`Sobra: ${leftoverHeight} cm`, textX, textY);
     }
 
-    pdf.save('plano-de-corte.pdf');
+    pdf.save('Plano-de-Corte.pdf');
   };
 
   return (
@@ -136,7 +149,7 @@ const App = () => {
               type="number"
               value={paperHeight}
               onChange={(e) => setPaperHeight(Number(e.target.value))}
-              addonBefore="Altura:"
+              addonBefore="Papel - Altura:"
               style={{ marginBottom: '10px' }}
             />
           </Col>
@@ -156,7 +169,18 @@ const App = () => {
               type="number"
               value={cutHeight}
               onChange={(e) => setCutHeight(Number(e.target.value))}
-              addonBefore="Altura:"
+              addonBefore="Corte - Altura:"
+              style={{ marginBottom: '10px' }}
+            />
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Input
+              type="number"
+              value={paperQuantity}
+              onChange={(e) => setPaperQuantity(Number(e.target.value))}
+              addonBefore="Qtd. de Folhas:"
               style={{ marginBottom: '10px' }}
             />
           </Col>
